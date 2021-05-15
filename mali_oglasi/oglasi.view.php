b088c32abe2c6bb53ed556ca3857474f9e721d88
Aleksa Obradovic <obradovic.aleksa@gmail.com>
2021-05-15 23:20:02 +0200

    kartice popunjene

--- mali_oglasi/oglasi.view.php
+++ mali_oglasi/oglasi.view.php
@@ -11,9 +11,25 @@
                 <?php foreach($oglasi as $oglas): ?>
                     <div class="col-4">
                         <div class="card mb-2 mt-2">
-                            <div class="card-header"></div>
-                            <div class="card-body"></div>
-                            <div class="card-footer"></div>
+                            <div class="card-header">
+                                <a href="" class="btn btn-secondary btn-sm">
+                                    <?php echo $oglas['category']; ?>
+                                </a>
+                            </div>
+                            <div class="card-body text-center">
+                                <h5><?php echo $oglas['title']; ?></h5>
+                                <a href="" class="btn btn-light btn-sm">
+                                    Vidi oglas
+                                </a>
+                            </div>
+                            <div class="card-footer">
+                                <a href="" class="btn btn-warning btn-sm float-left">
+                                    <?php echo $oglas['name'];?>
+                                </a>
+                                <a href="" class="btn btn-danger btn-sm float-right">
+                                    <?php echo $oglas['price'];?>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 <?php endforeach; ?>
