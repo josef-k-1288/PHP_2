3dc9790b1091d8adb0e24b4e44120fd21de8a7a2
Aleksa Obradovic <obradovic.aleksa@gmail.com>
2021-05-15 22:58:56 +0200

    oglasi kartice

--- mali_oglasi/oglasi.view.php
+++ mali_oglasi/oglasi.view.php
@@ -1,16 +1,22 @@
 <?php require_once "partials/head.php" ?>
 <?php require_once "partials/navbar.php" ?>
 
-<?php $oglasi = getAll(); 
-    dd($oglasi);
-?>
+<?php $oglasi = getAll(); ?>
 
 <div class="conteiner">
     <div class="row">
         <div class="col-10-offset-1">
-            <h1 class="display-4 text-center"></h1>
+            <h1 class="display-4 text-center">Svi Oglasi</h1>
             <div class="row">
-
+                <?php foreach($oglasi as $oglas): ?>
+                    <div class="col-4">
+                        <div class="card mb-2 mt-2">
+                            <div class="card-header"></div>
+                            <div class="card-body"></div>
+                            <div class="card-footer"></div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
